41293d0252e5673d6ee1d12c07b798420b764127
HARSHIT TOMAR <tomarharshit30@gmail.com>
2023-06-04 15:34:17 +0530

    added message in first.cpp

--- first.cpp
+++ first.cpp
@@ -1,11 +1,12 @@
 #include <iostream>  
 using namespace std;  
 void main() {  
-    int num1, num2, sum;  
+    int num1, num2;
+    int sum=0;
     cout << "Enter the first number: ";  
     cin >> num1;  
     cout << "Enter the second number: ";  
     cin >> num2;  
     sum = num1 + num2;  
-    cout << "The sum of the two numbers is: " << sum;   
+    cout  << sum;   
 }  
