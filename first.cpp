03eac40521e954de29222c111e904b8574d82493
HARSHIT TOMAR <92741840+HarshitTomar1015@users.noreply.github.com>
2023-06-04 15:13:10 +0530

    initial commit

---
+++ first.cpp
@@ -0,0 +1,11 @@
+#include <iostream>  
+using namespace std;  
+void main() {  
+    int num1, num2, sum;  
+    cout << "Enter the first number: ";  
+    cin >> num1;  
+    cout << "Enter the second number: ";  
+    cin >> num2;  
+    sum = num1 + num2;  
+    cout << "The sum of the two numbers is: " << sum;   
+}  
